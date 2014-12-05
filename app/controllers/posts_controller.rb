03e3a80e07f97da5603bef222c6595560865dbcd
Zotov-Nikita <apaxuc13@gmail.com>
2014-12-05 14:34:13 +0400

    lesson 28.11.14

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,10 @@
 class PostsController < ApplicationController
-    before_filter :post, only: [:show, :edit, :update, :destroy]
+    before_action :post, only: [:show, :edit, :update, :destroy]
+	before_action :authenticate_user!, only: [:show]
 
     def index
-    # instance param
-    @posts = Post.all
+    
+    	@posts = Post.all
 
     end
 
@@ -11,12 +12,16 @@ class PostsController < ApplicationController
     end
 
     def new
-        # @post = Post.new
+         @post = Post.new
+	
     end
 
     def create
         puts params
+	#@post=Post.new(post_params)
+	#@post.user_id = current_user
         Post.create(post_params)
+	#@post = current_user.posts.build(post_params)
         # flash[:notice] = "Post was successfully created"
         redirect_to posts_path, notice: "Post was successfully created"
     end
@@ -36,7 +41,7 @@ class PostsController < ApplicationController
 
     def destroy
         @post.destroy
-        flash[:notice] = "Post was successfully deleted"
+        flash[:notice] = "Post was successfully destroyed"
         redirect_to posts_path
     end
 
@@ -46,7 +51,7 @@ class PostsController < ApplicationController
         end
 
         def post_params
-            params.require(:post).permit(:title, :text)
+            params.require(:post).permit(:title, :text).merge(user: current_user)
         end
 
         helper_method :post
